2997843791f881d0afc5151116c2d28554cea070
Yasin <mustafayasinemir@gmail.com>
2024-05-23 01:43:15 +0300

    OrderService

---
+++ eCommerce.UI/Services/OrderService/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.UI.Services.OrderService
+{
+    public interface IOrderService
+    {
+    }
+}
---
+++ eCommerce.UI/Services/OrderService/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using eCommerce.UI.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace eCommerce.UI.Services.OrderService
+{
+    public class OrderService : ApiService
+    {
+        public async Task<bool> PlaceOrder(Order order)
+        {
+            var json = JsonConvert.SerializeObject(order);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync(AppSettings.ApiUrl + "api/orders", content);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<List<OrderByUser>> GetOrdersByUser(int userId)
+        {
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "api/orders/ordersbyuser/" + userId);
+            return JsonConvert.DeserializeObject<List<OrderByUser>>(response);
+        }
+
+        public async Task<List<OrderDetail>> GetOrderDetails(int orderId)
+        {
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "api/orders/orderdetails/" + orderId);
+            return JsonConvert.DeserializeObject<List<OrderDetail>>(response);
+        }
+    }
+}
+
