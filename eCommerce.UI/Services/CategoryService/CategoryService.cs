73c1fb9bd36b408112bb6830b6f3dd8a9fade258
Yasin <mustafayasinemir@gmail.com>
2024-05-23 01:42:59 +0300

    Category Service

---
+++ eCommerce.UI/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,15 @@
+﻿using eCommerce.UI.Models;
+using Newtonsoft.Json;
+
+namespace eCommerce.UI.Services.CategoryService
+{
+    public class CategoryService : ApiService
+    {
+        public async Task<List<Category>> GetCategories()
+        {
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "api/categories");
+            return JsonConvert.DeserializeObject<List<Category>>(response);
+        }
+    }
+
+}
---
+++ eCommerce.UI/Services/CategoryService/ICategoryServicecs.cs
@@ -0,0 +1,6 @@
+﻿namespace eCommerce.UI.Services.CategoryService
+{
+    public interface ICategoryServicecs
+    {
+    }
+}
