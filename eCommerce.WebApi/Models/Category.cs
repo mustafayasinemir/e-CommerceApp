536281ecb0197dd7cf9e018c5810e2d3e9c297e7
Yasin <mustafayasinemir@gmail.com>
2024-05-23 01:45:26 +0300

    Model Update

--- eCommerce.WebApi/Models/Category.cs
+++ eCommerce.WebApi/Models/Category.cs
@@ -5,9 +5,8 @@ namespace eCommerce.Api.Models
     public class Category
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? ImageUrl { get; set; }
-
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
 
         [JsonIgnore]
         public ICollection<Product>? Products { get; set; }  
