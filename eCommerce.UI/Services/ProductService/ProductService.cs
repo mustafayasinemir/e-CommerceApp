4a3b4afe13e2bd1be940a3aee69f65cacd120ef4
Yasin <mustafayasinemir@gmail.com>
2024-05-23 21:01:26 +0300

    ProductService edit

--- eCommerce.UI/Services/ProductService/ProductService.cs
+++ eCommerce.UI/Services/ProductService/ProductService.cs
@@ -1,5 +1,6 @@
 ﻿using eCommerce.UI.Models;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace eCommerce.UI.Services.ProductService
 {
@@ -11,11 +12,24 @@ namespace eCommerce.UI.Services.ProductService
             return JsonConvert.DeserializeObject<List<Product>>(response);
         }
 
+        public async Task<List<Product>> GetProductsAdmin()
+        {
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl+"api/admin/products");
+            return JsonConvert.DeserializeObject<List<Product>>(response);
+        }
+
 
         public async Task<ProductDetail> GetProductDetail(int productId)
         {
             var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "api/products/" + productId);
             return JsonConvert.DeserializeObject<ProductDetail>(response);
+        }
+        public async Task<bool> UpdateProductAsync(int id, Product product)
+        {
+            var json = JsonConvert.SerializeObject(product);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PutAsync(AppSettings.ApiUrl+"api/admin/products/{productId}", content);
+            return response.IsSuccessStatusCode;
         }
 
 
